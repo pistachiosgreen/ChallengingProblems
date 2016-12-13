e8f9e597f4a4f589d0f8c7f0c932a9c5d031b84f
pistachiosgreen <jsguo9@gmail.com>
2016-12-12 23:24:00 -0800

    three sum

--- ThreeSum.java
+++ ThreeSum.java
@@ -1,95 +1,123 @@
-import java.util.*;
 public class Solution {
-    //225/311 with string
-    //Input:[-2,-7,9,0,4,-1,9,-3,-9,8,9,2,-5,-6,2,-10,-3,1,2]
-    //Expected Output:[[-10,1,9],[-10,2,8],[-9,0,9],[-9,1,8],[-7,-2,9],[-7,-1,8],
-    //[-6,-3,9],[-6,-2,8],[-6,2,4],[-5,-3,8],[-5,1,4],[-3,-1,4],[-3,1,2],[-2,0,2],[-1,0,1]]
-    //what I got: [[-2,0,2],[-6,-2,8],[-7,-2,9],[-7,-1,8],[-10,1,9],[-6,-3,9],[-9,0,9],[-6,2,4],
-    //[-5,1,4],[-3,-1,4],[-3,1,2],[-5,-3,8],[-9,1,8],[-10,2,8]]
-    /* 238/311 with hashmap
-    [13,4,-6,-7,-15,-1,0,-1,0,-12,-12,9,3,-14,-2,-5,-6,7,8,2,-4,6,-5,
-    -10,-4,-9,-14,-14,12,-13,-7,3,7,2,11,7,9,-4,13,-6,-1,-14,-12,9,9,
-    -6,-11,10,-14,13,-2,-11,-4,8,-6,0,7,-12,1,4,12,9,14,-4,-3,11,10,
-    -9,-8,8,0,-1,1,3,-15,-12,4,12,13,6,10,-4,10,13,12,12,-2,4,
-    7,7,-15,-4,1,-15,8,5,3,3,11,2,-11,-12,-14,5,-1,9,0,-12,6,-1,1,1,2,-3]
-    */
-
     public List<List<Integer>> threeSum(int[] nums) {
-    
-    	ArrayList<List<Integer>> result = new ArrayList<List<Integer>>();
-        HashMap<Integer, Integer> hmap = new HashMap<Integer,Integer>();
-        String storage = "";
+        ArrayList<List<Integer>> result = new ArrayList<List<Integer>>();
+        HashMap<Integer, Integer> negmap = new HashMap<Integer,Integer>();
+        HashMap<Integer, Integer> posmap = new HashMap<Integer,Integer>();
+        ArrayList<Integer> nlist = new ArrayList<Integer>();
+        ArrayList<Integer> plist = new ArrayList<Integer>();
+        int numZeros = 0;
+
         for (int x: nums){
-        	if (hmap.containsKey(x)){
-        		int lastval = hmap.get(x);
-        		hmap.put(x,lastval + 1); 
-        	}
-        	else{
-        		hmap.put(x,1);
-        	}
-        }
-        Arrays.sort(nums);
-        int index = nums.length/2;
-        if (nums[index] < 0){
-
-            while(index < nums.length && temp >= 0){
-                temp = nums[index];
-                index++;
-            }
-        }
-        if (nums[index] > 0){
-            while(index > 0 && temp <= 0){
-                temp = nums[index];
-                index--;
+            if(x == 0){
+                numZeros++;
             }
 
+            if(x < 0){
+                if(negmap.containsKey(x)){
+                    int lastval = negmap.get(x);
+                    negmap.put(x,lastval + 1); 
+                 }
+                else{
+                    negmap.put(x,1);
+                    nlist.add(x);
+                }
+            }
+            else{
+
+                if(posmap.containsKey(x)){
+                    int lastval = posmap.get(x);
+                    posmap.put(x,lastval + 1); 
+                 }
+                else{
+                    posmap.put(x,1);
+                    plist.add(x);
+                }
+            }
+        }
+        
+
+        for (int i = 0; i < nlist.size(); i++){
+            int current = nlist.get(i);
+            if (negmap.get(current) > 1){
+                if (posmap.containsKey(current * -2)){
+                    ArrayList<Integer> toAdd = new ArrayList<Integer>();
+                    toAdd.add(current);
+                    toAdd.add(current);
+                    toAdd.add(current * -2);
+                    result.add(toAdd);
+                }
+            }
+
+           /* if (numZeros >= 1 && posmap.containsKey(current * -1)){
+                    ArrayList<Integer> toAdd = new ArrayList<Integer>();
+                    toAdd.add(current);
+                    toAdd.add(0);
+                    toAdd.add(current * -1);
+                    result.add(toAdd);
+            }
+            */
+            for (int j = i + 1; j < nlist.size(); j++){
+                int left = nlist.get(i);
+                int right = nlist.get(j);
+                int sum = left + right;
+                if (left != 0 && right != 0){
+                if (posmap.containsKey(0 - sum)){
+                    ArrayList<Integer> toAdd = new ArrayList<Integer>();
+                    toAdd.add(nlist.get(i));
+                    toAdd.add(nlist.get(j));
+                    toAdd.add(0 - sum);
+                    result.add(toAdd);
+                }
+                }
+            }
+
         }
 
-        for (int i = 0; i < nums.length - 2; i++){
-            for (int j = nums.length - 1; j > i; j--){
-            	int sum = nums[i] + nums[j];
-            	int find = 0 - sum;
-            	if(hmap.containsKey(find)){
-            		int num = hmap.get(find);
-            		if (find == nums[i]){
-            			num--;
-            		}
-            		if (find == nums[j]){
-            			num--;
-            		}
-            		if (num > 0){
-            			List<Integer> toAdd = new ArrayList<Integer>();
-            			toAdd.add(nums[i]);
-            			toAdd.add(nums[j]);
-            			toAdd.add(find);
-                        Collections.sort(toAdd);
-                        String temp = "";
-                        for (int z: toAdd){
-                            temp += String.valueOf(z);
-                        }
-                        if (!storage.contains(temp)){
-                            result.add(toAdd);
-                        }
-                        /*
-            			boolean duplicate = false;
-            			Collections.sort(toAdd);
-            			for (List y: result){
-            				if (y.equals(toAdd)){
-            					duplicate = true;
-            					break;
-            				}
-            			}
-            			if (duplicate == false){
-            				result.add(toAdd);
-            			}
+           for (int i = 0; i < plist.size(); i++){
+            int current = plist.get(i);
+            if (posmap.get(current) > 1){
+                if (negmap.containsKey(current * -2)){
+                    ArrayList<Integer> toAdd = new ArrayList<Integer>();
+                    toAdd.add(current);
+                    toAdd.add(current);
+                    toAdd.add(current * -2);
+                    result.add(toAdd);
+                }
+            }
 
-                        */
-            		}
-
-            	}
+            if (numZeros >= 1 && negmap.containsKey(current * -1)){
+                    ArrayList<Integer> toAdd = new ArrayList<Integer>();
+                    toAdd.add(current);
+                    toAdd.add(0);
+                    toAdd.add(current * -1);
+                    result.add(toAdd);
+            }
+            for (int j = i + 1; j < plist.size(); j++){
+                int left = plist.get(i);
+                int right = plist.get(j);
+                int sum = left + right;
+                if (left != 0 && right != 0){
+                if (negmap.containsKey(0 - sum)){
+                    ArrayList<Integer> toAdd = new ArrayList<Integer>();
+                    toAdd.add(plist.get(i));
+                    toAdd.add(plist.get(j));
+                    toAdd.add(0 - sum);
+                    result.add(toAdd);
+                }
+            }
 
         }
-        }
+           }
+
+           if (numZeros >= 3){
+               ArrayList<Integer> toAdd = new ArrayList<Integer>();
+               toAdd.add(0);
+               toAdd.add(0);
+               toAdd.add(0);
+               result.add(toAdd);
+           }
+
         return result;
-    }
+        
+}
 }
